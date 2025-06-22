f1478e4d520e1e3c9da59d95ccbac2b6a61c5682
sunku <prashanthsunku9@gmail.com>
2025-06-22 16:40:04 +0530

    changes made in new.java by java branch

--- New.java
+++ New.java
@@ -1,5 +1,10 @@
 class New{
     public static void main(String args[]){
         System.out.println("new branch without pulling");
+        System.out.println("changes made by java branch");
+        
+
+
+        
     }
 }
